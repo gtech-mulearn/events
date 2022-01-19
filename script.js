cdaabadf8cd5d7c057c18b7bc87b82b0a89af424
Aswin Asok <aswinasokofficial@gmail.com>
2022-01-19 21:36:41 +0530

    Container Switching is also working

--- script.js
+++ script.js
@@ -7,4 +7,17 @@ const swap_header = () => {
 
   active_element.classList.add("hide");
   active_element.classList.remove("show");
+
+  swap_container();
 };
+
+const swap_container = () => {
+    inactive_container = document.getElementsByClassName("hide")[1];
+    active_container = document.getElementsByClassName("show")[1];
+  
+    inactive_container.classList.remove("hide");
+    inactive_container.classList.add("show");
+  
+    active_container.classList.add("hide");
+    active_container.classList.remove("show");
+  };
